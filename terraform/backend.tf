70a17ad9ffda8ebfefbf93c842fa05de29bca339
takxxx-infra <takxxxaaa+aws1@gmail.com>
2026-03-16 21:59:06 +0900

    update: edit backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "ecs-bg-deployment-682120332115"
+    bucket       = "ecs-bg-deployment-xxxxxxxxxxxx"
     key          = "tfstate"
     region       = "ap-northeast-1"
     use_lockfile = true
